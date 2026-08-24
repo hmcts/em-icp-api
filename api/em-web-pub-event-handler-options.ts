f7a9351c41d276f2658cba9ccd3bd78d0d1bf326
Olu <142989683+olusegz07@users.noreply.github.com>
2026-08-24 16:44:10 +0100

    code tidy

--- api/em-web-pub-event-handler-options.ts
+++ api/em-web-pub-event-handler-options.ts
@@ -24,16 +24,9 @@ export class EmWebPubEventHandlerOptions implements WebPubSubEventHandlerOptions
   handleConnect = async (connectRequest: ConnectRequest, connectResponse: ConnectResponseHandler) => {
     const origin = this.getOriginHeader(connectRequest);
     if (!this.isOriginAllowed(origin)) {
-      console.log("ICP Web PubSub origin rejected", {
-        origin,
-        allowedOrigin: this.allowedOrigin,
-        headerNames: Object.keys(connectRequest.headers || {}),
-        contextOrigin: connectRequest.context?.origin,
-      });
       connectResponse.fail(401, "Origin not authorized to access session");
       return;
     }
-    console.log("ICP Web PubSub origin allowed", { origin, allowedOrigin: this.allowedOrigin });
 
     if (connectRequest.claims.role) {
       const roles = this.extractRolesFromConnectRequest(connectRequest);
